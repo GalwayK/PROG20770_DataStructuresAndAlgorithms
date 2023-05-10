35b15e959f731af75b173e304fab1654ab82ed73
Kyle Galway <kyligalway@gmail.com>
2023-05-09 22:54:35 -0400

    Review class.

---
+++ Week1/Lesson1/Lesson1.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+// The include lines are used to import header files. 
+// System includes use <> and custom includes use " " 
+// Include statements are handled by the C preprofessor.
+
+const double NUM_PI = 355.0 / 113;
+
+void funcPrintName(int intRepetitions, char* strFirstName)
+{
+	if (intRepetitions > 0)
+	{
+		funcPrintName(intRepetitions - 1, strFirstName);
+		printf("%d. Hello %s\n", intRepetitions, strFirstName);
+	}
+}
+
+void funcMultiplyPi()
+{
+	double numMultiplier;
+	
+	printf("Please enter a number to multiply by Pi: ");
+	int intResult = scanf("%lf", &numMultiplier);
+	
+	// Flush standard input of extra entries.
+	fflush(stdin);
+	
+	if (intResult < 1)
+	{
+		printf("Error in input!");
+	}
+	else 
+	{
+		printf("Pi multiplied by %f is %f", numMultiplier, NUM_PI * numMultiplier);
+	}
+}
+
+// Main function 
+int main(int argc, char** argv)
+{
+	printf("Please enter your name: ");
+	
+	// Strings are just arrays of char.
+	
+	int intFirstNameLength = 255;
+	char strFirstName[intFirstNameLength];
+	char strFirstNameLength[255];
+	sprintf(strFirstNameLength, "%d", intFirstNameLength);
+	char strTemplate[255] = "%";
+	strcat(strTemplate, strFirstNameLength);
+	strcat(strTemplate, "s");
+	
+	scanf(strTemplate, &strFirstName);
+//	
+//	// Must use print f
+	funcPrintName(10, strFirstName);
+//	funcMultiplyPi();
+//	printf("Size of int is %d bytes\n", sizeof(int));
+//	printf("Size of long int is %d bytes\n", sizeof(long int));
+	return 0;
+}
+
+// Definitions
+/*
+Preprocessor:
+Linker:  
+Source File: 
+Header File: 
+Object File: 
+*/
+
+// Four Main Properties of Variables 
+/*
+Data Type: 
+Name: 
+Value: 
+Scope: 
+*/
+
+// Data Types in C
+/*
+char: 8 bits (1 byte)
+short int: 16 bit numbers
+int: 32 bit numbers.
+long int: 32 to 64 bit numbers.
+long long int: 64 bit numbers.
+float: 32 bit decimal numbers with 8 digits of precision.
+double: 64 bit decimal numbers with 16 digits of precision.
+
+C numbers can be signed or unsigned. 
+Unsigned numbers can hold twice as many values, but cannot be negative.
+*/
+
+/*
+In C we should always give a variable an initial value.
+
+Static in C only means that we can only see a variable inside its module. 
+This is not the same as Java - C is not an object oriented language.
+*/
---
+++ Week1/Lesson1/scope.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 2;
+	int j = 4;
+	int k;
+
+	{	// Open brace starts a new scope
+		int i = 3;
+		k = 7;
+
+		printf("i = %d, j = %d, k = %d\n", i, j, k);
+	}
+
+	printf("i = %d, j = %d\n", i, j);
+
+	//Un-comment the printf that’s commented out and try  compiling
+
+	 printf("k=%d\n", k);  
+
+	return 0;
+}
